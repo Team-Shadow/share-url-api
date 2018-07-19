604d8c182e3b954f2295b8f88bd50dbca653f432
严俊东 <1172482914@qq.com>
2018-07-19 22:18:34 +0800

    github 登入功能

--- router/index.js
+++ router/index.js
@@ -5,10 +5,10 @@ const User = require('../model/user_schema')
 router.get('/', async ctx => {
     ctx.body = '<h1>share-url-api</h1>'
 })
-    .get('/github_callback', githubCallback)
+    .get('/api/github_callback', githubCallback)
     .get('/api/user', async ctx => {
         let user = ctx.session.user
-        ctx.body = user
+        ctx.body = { data: user }
     })
 
 /**
@@ -16,7 +16,7 @@ router.get('/', async ctx => {
  */
 const client_id = '274df6a3dc60b0dd834c'
 const client_secret = 'e8dfc09c2a5544087f4fc01c646d3f57b302e0f5'
-async function githubCallback(ctx) {
+async function githubCallback (ctx) {
     let code = ctx.query.code
     let { data: tokenStr } = await axios.post('https://github.com/login/oauth/access_token', {
         client_id,
@@ -24,10 +24,11 @@ async function githubCallback(ctx) {
         code,
     })
     let { data: githubUser } = await axios.get('https://api.github.com/user?' + tokenStr)
+    
     /**
      * 查找用户，无 - 注册并登入  有 - 登入
      */
-    let findUser = User.findOne({ github_id: githubUser.id })
+    let findUser = await User.findOne({ github_id: githubUser.id })
     if (findUser) { // 已存在
         ctx.session.user = findUser
         // ctx.body = findUser
@@ -37,7 +38,7 @@ async function githubCallback(ctx) {
         user.github = githubUser
         user.name = githubUser.name
         let ret = await user.save()
-        ctx.session.user = ret.user
+        ctx.session.user = user
         // ctx.body = user
     }
     ctx.body = `<script>location.href = '/'</script>`
